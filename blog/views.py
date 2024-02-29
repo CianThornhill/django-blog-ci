301eb48d05f150b872ddf53b802f21abe3275457
Cian Thornhill <cianthornhill95@gmail.com>
2024-02-29 11:16:15 +0000

    add form testing

--- blog/views.py
+++ blog/views.py
@@ -37,7 +37,6 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -50,7 +49,6 @@ def post_detail(request, slug):
             )
     
     comment_form = CommentForm()
-    print("About to render template")
 
     return render(
     request,
